764f8f429c18c9beff0abd16abbae57e524b3213
poudelpatwit <prafulpoudel21@gmail.com>
2023-11-14 13:01:14 -0500

    header component complete

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="title">Know Your Weather</h1>
+        </header>
+    );
+};
+
+export default Header;
