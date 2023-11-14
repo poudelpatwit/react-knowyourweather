2b88226e2a3fc3ea3519e52fd0468aa045e9f9f7
poudelpatwit <prafulpoudel21@gmail.com>
2023-11-14 13:01:26 -0500

    import header on the main file

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {Helmet, HelmetProvider} from "react-helmet-async";
+import React, { useState } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 //import Footer here
 import Footer from "../components/footer/footer";
@@ -13,28 +13,29 @@ import SearchBar from "../components/searchBar/SearchBar";
 
 import WeatherForFuture from "../components/weatherCard/WeatherForFuture";
 import WeatherDaily from "../components/weatherCard/WeatherDaily";
-import {default as axios} from "axios";
+
+import { default as axios } from "axios";
+import Header from "../components/header/header";
 
 function Main() {
 
-        //openweathermap api key
-        var OPEN_WEATHER_API_KEY = "fe4feefa8543e06d4f3c66d92c61b69c";
-        var GEO_CODE_API = "2c857ccaba884d4b8378f6de0e309bfa";
+    //openweathermap api key
+    var OPEN_WEATHER_API_KEY = "fe4feefa8543e06d4f3c66d92c61b69c";
+    var GEO_CODE_API = "2c857ccaba884d4b8378f6de0e309bfa";
 
-        //first component variables
-        //this passes the city and calls the update city function
-        const [city, updateCity] = useState();
+    //first component variables
+    //this passes the city and calls the update city function
+    const [city, updateCity] = useState();
 
-        //this stores the api response
-        const [weather, updateWeather] = useState();
+    //this stores the api response
+    const [weather, updateWeather] = useState();
 
-        //store nearby cities data
-        const [cityDetails, setNearbyCities] = useState();
+    //store nearby cities data
+    const [cityDetails, setNearbyCities] = useState();
 
-        //function to make the api call using the city name
-        /*const [weatherForTomorrow, updateWeatherForTomorrow] = useState(); */
-        const fetchWeather =  async (e) =>
-              {
+    //function to make the api call using the city name
+    /*const [weatherForTomorrow, updateWeatherForTomorrow] = useState(); */
+    const fetchWeather = async (e) => {
         const axios = require('axios').default;
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`,
         )
@@ -46,70 +47,71 @@ function Main() {
                 // Handle Error Here
                 console.error(err);
             });
-             }
+    }
 
-               //this will store our latitude and longitude
+    //this will store our latitude and longitude
 
-             //here we will call our api to get the nearby cities data
-             const getNearbyCities = async (e) => {
-                     const axios = require('axios').default;
-              axios.get(`https://api.openweathermap.org/data/2.5/find?lat=${weather?.coord?.lat}&lon=${weather?.coord?.lon}&cnt=3&appid=${OPEN_WEATHER_API_KEY}`,
-                     )
-                         .then(resp => {
-                             console.log(resp.data);
-                             setNearbyCities(resp.data);
-                             //store the api response
-                         })
-                         .catch(err => {
-                             // Handle Error Here
-                             console.error(err);
-                         });
-             }
-
-
-            /*
-
-             //this will be the function to get lat long from the input city
-             const getLatLong = async (e) =>{
-
-             }
-
-             //create a function to get weather for tomorrow using a different field in the link
-           /*
-        const fetchWeatherForTomorrow = async (e) => {
-            navigator.geolocation.getCurrentPosition(function(position) {
-
-                console.log("Latitude is :", position.coords.latitude);
-                const lat = position.coords.latitude;
-                console.log("Longitude is :", position.coords.longitude);
-                const long = position.coords.longitude
+    //here we will call our api to get the nearby cities data
+    const getNearbyCities = async (e) => {
+        const axios = require('axios').default;
+        axios.get(`https://api.openweathermap.org/data/2.5/find?lat=${weather?.coord?.lat}&lon=${weather?.coord?.lon}&cnt=3&appid=${OPEN_WEATHER_API_KEY}`,
+        )
+            .then(resp => {
+                console.log(resp.data);
+                setNearbyCities(resp.data);
+                //store the api response
+            })
+            .catch(err => {
+                // Handle Error Here
+                console.error(err);
             });
-            const axios = require('axios').default;
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
-            )
-                .then(resp => {
-                    console.log(resp.data);
-                    updateWeatherForTomorrow(resp.data);
-                })
-                .catch(err => {
-                    // Handle Error Here
-                    console.error(err);
-                });
+    }
 
-        }
-            */
+
+    /*
+
+     //this will be the function to get lat long from the input city
+     const getLatLong = async (e) =>{
+
+     }
+
+     //create a function to get weather for tomorrow using a different field in the link
+   /*
+const fetchWeatherForTomorrow = async (e) => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        console.log("Latitude is :", position.coords.latitude);
+        const lat = position.coords.latitude;
+        console.log("Longitude is :", position.coords.longitude);
+        const long = position.coords.longitude
+    });
+    const axios = require('axios').default;
+    axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
+    )
+        .then(resp => {
+            console.log(resp.data);
+            updateWeatherForTomorrow(resp.data);
+        })
+        .catch(err => {
+            // Handle Error Here
+            console.error(err);
+        });
+
+}
+    */
     return (
-        <div className = "mainPage">
-         <SearchBar  updateCity = {updateCity} fetchWeather = {fetchWeather}  getNearbyCities = {getNearbyCities}/>
-         <WeatherCard  weather = {weather} city = {city} />
-         <WeatherForFuture weather = {weather}   />
-         <WeatherDaily/>
-         <div className = "surroundingCities">
-         <WeatherCard2  cityDetails = {cityDetails}/>
-         <WeatherCard3  cityDetails = {cityDetails}/>
-         </div>
-         <Footer/>
+        <div className="mainPage">
+            <Header />
+            <SearchBar updateCity={updateCity} fetchWeather={fetchWeather} getNearbyCities={getNearbyCities} />
+            <WeatherCard weather={weather} city={city} />
+            <WeatherForFuture weather={weather} />
+            <WeatherDaily />
+            <div className="surroundingCities">
+                <WeatherCard2 cityDetails={cityDetails} />
+                <WeatherCard3 cityDetails={cityDetails} />
+            </div>
+            <Footer />
         </div>
-            );
+    );
 }
 export default Main;
