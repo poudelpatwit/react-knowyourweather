d861a1f2b3dfa3e0242d555fda5c890a6c2ce969
poudelpatwit <prafulpoudel21@gmail.com>
2023-11-14 13:44:08 -0500

    slight css updates

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,18 @@
 import React from "react";
-import {Button, Form, Input} from "semantic-ui-react";
+import { Button, Form, Input } from "semantic-ui-react";
 
 const SearchBar = (props) => {
-    const {updateCity, fetchWeather, getNearbyCities} = props;
+    const { updateCity, fetchWeather, getNearbyCities } = props;
     return (
-        <div className="searchDiv">
-            <Form onSubmit = {fetchWeather}>
-            <input className="searchBar"  onChange={(e) => updateCity(e.target.value)} placeholder = "Enter your city: "/>
-            <button className="searchButton" type={"submit"}>Submit </button>
-            </Form>
-             <div className="searchDiv2">
-            <button className = "searchButton2" onClick = {getNearbyCities}> Nearby Weather Data </button>
+        <div className="search-container">
+            <div className="searchDiv">
+                <Form onSubmit={fetchWeather} className="weather-form">
+                    <input className="searchBar" onChange={(e) => updateCity(e.target.value)} placeholder="Enter your city" /><br></br>
+                    <button className="searchButton" type={"submit"}>Submit </button>
+                </Form>
+                <div className="searchDiv2">
+                    <button className="searchButton2" onClick={getNearbyCities}> Nearby Weather Data </button>
+                </div>
             </div>
         </div>
     );
